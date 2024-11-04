195a367643443b032a97259e94e6991d01a203bc
SebastianV75 <92833858+SebastianV75@users.noreply.github.com>
2024-11-04 20:40:19 -0300

    Add files via upload

---
+++ EVA3_1_METODOS/src/main/java/com/mycompany/eva3_1_metodos/EVA3_1_METODOS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_1_metodos;
+
+/**
+ *
+ * @author sebas
+ */
+public class EVA3_1_METODOS {
+
+    public static void main(String[] args) {
+         //USO DE LOS METODOS:
+         //INVOCACION O LLAMADA
+         imprimirMensaje("Hola mundo!");
+    }
+    
+    //DECLARACION
+    // header  (encabezado)
+    //imprimir un mensaje
+    // JAVA: iniciar en minusculas, verbo, siguientes palabras inician en mayusculas
+    public static void imprimirMensaje(String mensaje){
+        System.out.println("TU MENSAJE: " + mensaje);
+    }
+}
